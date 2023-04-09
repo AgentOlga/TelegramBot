1e6e835946a21c025d7b2fa20dd4048b9fab49f7
AgentOlga <agentolga@gmail.com>
2023-04-09 14:18:07 +0200

    Create TelegramBot HomelessPetsHouseBot

--- src/main/java/com/example/telegrambot/TelegramBotApplication.java
+++ src/main/java/com/example/telegrambot/TelegramBotApplication.java
@@ -2,12 +2,16 @@ package com.example.telegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 public class TelegramBotApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TelegramBotApplication.class, args);
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(new Bot());
     }
 
 }
