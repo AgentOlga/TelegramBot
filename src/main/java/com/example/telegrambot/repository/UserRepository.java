eafb295553069b5baad65b884ef14a3b2d373350
Aleksey2710 <Aleksey_2710@mail.ru>
2023-04-26 14:52:17 +0300

    созданы или изменены: ShelterTest, TelegramBotUpdatesListener, UserRepository, UserRequestServiceImpl, UserServiceImpl, UserTest

--- src/main/java/com/example/telegrambot/repository/UserRepository.java
+++ src/main/java/com/example/telegrambot/repository/UserRepository.java
@@ -16,54 +16,59 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     /**
      * Выводим пользователя по идентификатору в телеграмм
+     *
      * @param userId идентификатор в телеграмм
      * @return найденный пользователь
      */
     //@Modifying
     @Query("SELECT u FROM User u WHERE u.userId = :user_id")
-    User findByUserId(@Param("user_id") long userId);
+    User findByUserId(@Param("user_id") Long userId);
 
     /**
      * Изменяем пользователя до гостя
-     * @param userId идентификатор в телеграмм
-     * @param firstName имя гостя
-     * @param lastName фамилия гостя
+     *
+     * @param userId      идентификатор в телеграмм
+     * @param firstName   имя гостя
+     * @param lastName    фамилия гостя
      * @param phoneNumber телефон гостя
-     * @param carNumber номер машины гостя
-     * @param userType тип пользователя
-     * @param userStatus статус пользователя
+     * @param carNumber   номер машины гостя
+     * @param userType    тип пользователя
+     * @param userStatus  статус пользователя
      */
-    //@Modifying
-    @Query("UPDATE User u SET u.firstName = :first_name, " +
+//    @Modifying
+    @Query("UPDATE User u SET " +
+            "u.firstName = :first_name, " +
             "u.lastName = :last_name," +
             "u.phoneNumber = :phone_number," +
             "u.carNumber = :car_number," +
             "u.shelterType = :shelter_type," +
             "u.userType = :user_type," +
             "u.userStatus = :user_status" +
-            " where u.userId = :user_id")
-    void updateUserInGuestById(@Param("user_id") long userId,
-                               @Param("first_name") String firstName,
-                               @Param("last_name") String lastName,
-                               @Param("phone_number") String phoneNumber,
-                               @Param("car_number") String carNumber,
-                               @Param("shelter_type") ShelterType shelterType,
-                               @Param("user_type") UserType userType,
-                               @Param("user_status") UserStatus userStatus);
+            " WHERE u.userId = :user_id")
+    void updateUserInGuestById(
+            @Param("first_name") String firstName,
+            @Param("last_name") String lastName,
+            @Param("phone_number") String phoneNumber,
+            @Param("car_number") String carNumber,
+            @Param("shelter_type") ShelterType shelterType,
+            @Param("user_type") UserType userType,
+            @Param("user_status") UserStatus userStatus,
+            @Param("user_id") Long userId);
 
     /**
      * Изменяем гостя до усыновителя/волонтера
-     * @param userId идентификатор в телеграмм
-     * @param firstName имя усыновителя/волонтера
-     * @param lastName фамилия усыновителя/волонтера
+     *
+     * @param userId      идентификатор в телеграмм
+     * @param firstName   имя усыновителя/волонтера
+     * @param lastName    фамилия усыновителя/волонтера
      * @param phoneNumber номер телефона усыновителя/волонтера
-     * @param carNumber номер машины усыновителя/волонтера
-     * @param userType тип пользователя
-     * @param userStatus статус пользователя
-     * @param email эл.почта усыновителя/волонтера
-     * @param address адрес усыновителя/волонтера
+     * @param carNumber   номер машины усыновителя/волонтера
+     * @param userType    тип пользователя
+     * @param userStatus  статус пользователя
+     * @param email       эл.почта усыновителя/волонтера
+     * @param address     адрес усыновителя/волонтера
      */
-    //@Modifying
+    @Modifying
     @Query("UPDATE User u SET u.firstName = :first_name, " +
             "u.lastName = :last_name," +
             "u.phoneNumber = :phone_number," +
@@ -73,7 +78,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
             "u.userStatus = :user_status," +
             "u.email = :email," +
             "u.address = :address" +
-            " where u.userId = :user_id")
+            " WHERE u.userId = :user_id")
     void updateGuestInAdopterById(@Param("user_id") long userId,
                                   @Param("first_name") String firstName,
                                   @Param("last_name") String lastName,
--- src/test/java/com/example/telegrambot/model/UserTest.java
+++ src/test/java/com/example/telegrambot/model/UserTest.java
@@ -4,6 +4,8 @@ import com.example.telegrambot.constants.ConstantValue;
 import com.example.telegrambot.constants.ShelterType;
 import com.example.telegrambot.constants.UserStatus;
 import com.example.telegrambot.constants.UserType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,11 +30,92 @@ class UserTest {
     private static final String EMPTY_VALUE = "";
     private static final String BLANK_VALUE = " ";
     private User user;
+    private User userCorrect;
+    private User userWrong;
+    private User guest;
+    private User guestCorrect;
+    private User guestWrong;
+    private User adopter;
+    private User adopterCorrect;
+    private User adopterWrong;
 
+
+    @BeforeEach
+    public void initTest() {
+        userCorrect = new User(CORRECT_USER_ID, CORRECT_USER_NAME, CORRECT_USER_TYPE,
+                CORRECT_USER_STATUS);
+
+        userWrong = new User(CORRECT_USER_ID, BLANK_VALUE, CORRECT_USER_TYPE,
+                CORRECT_USER_STATUS);
+
+        user = new User(CORRECT_USER_ID, CORRECT_USER_NAME, CORRECT_USER_TYPE,
+                CORRECT_USER_STATUS);
+
+        guestCorrect = new User(CORRECT_USER_ID, CORRECT_USER_NAME, CORRECT_FIRST_NAME,
+                CORRECT_LAST_NAME, CORRECT_PHONE_NUMBER, CORRECT_CAR_NUMBER,
+                CORRECT_SHELTER_TYPE, CORRECT_USER_TYPE, CORRECT_USER_STATUS);
+
+        guestWrong = new User(CORRECT_USER_ID, BLANK_VALUE, CORRECT_FIRST_NAME,
+                CORRECT_FIRST_NAME, CORRECT_PHONE_NUMBER, BLANK_VALUE,
+                CORRECT_SHELTER_TYPE, CORRECT_USER_TYPE, CORRECT_USER_STATUS);
+
+        guest = new User(CORRECT_USER_ID, CORRECT_USER_NAME, CORRECT_FIRST_NAME,
+                CORRECT_LAST_NAME, CORRECT_PHONE_NUMBER, CORRECT_CAR_NUMBER,
+                CORRECT_SHELTER_TYPE, CORRECT_USER_TYPE, CORRECT_USER_STATUS);
+
+        adopterCorrect = new User(CORRECT_USER_ID, CORRECT_USER_NAME, CORRECT_FIRST_NAME,
+                CORRECT_LAST_NAME, CORRECT_PHONE_NUMBER, CORRECT_CAR_NUMBER,
+                CORRECT_MAIL, CORRECT_ADDRESS,
+                CORRECT_SHELTER_TYPE, CORRECT_USER_TYPE, CORRECT_USER_STATUS);
+
+        adopterWrong = new User(CORRECT_USER_ID, BLANK_VALUE, CORRECT_FIRST_NAME,
+                CORRECT_LAST_NAME, CORRECT_PHONE_NUMBER, BLANK_VALUE,
+                CORRECT_MAIL, CORRECT_ADDRESS,
+                CORRECT_SHELTER_TYPE, CORRECT_USER_TYPE, CORRECT_USER_STATUS);
+
+        adopter = new User(CORRECT_USER_ID, CORRECT_USER_NAME, CORRECT_FIRST_NAME,
+                CORRECT_LAST_NAME, CORRECT_PHONE_NUMBER, CORRECT_CAR_NUMBER,
+                CORRECT_MAIL, CORRECT_ADDRESS,
+                CORRECT_SHELTER_TYPE, CORRECT_USER_TYPE, CORRECT_USER_STATUS);
+
+    }
+
+    @AfterEach
+    public void afterTest() {
+        System.out.println("Testing is finished!");
+    }
+
+    @Test
+    public void checkingForIncomingCorrectDataFromTheMethodAddUser() {
+        assertEquals(userCorrect, user);
+    }
+
+    @Test
+    public void CheckingForIncomingWrongDataFromTheMethodAddUser() {
+        assertNotEquals(userCorrect, userWrong);
+    }
+
+    @Test
+    public void checkingForIncomingCorrectDataFromTheMethodAddGuest() {
+        assertEquals(guestCorrect, guest);
+    }
+
+    @Test
+    public void CheckingForIncomingWrongDataFromTheMethodAddGuest() {
+        assertNotEquals(guestCorrect, guestWrong);
+    }
+    @Test
+    public void checkingForIncomingCorrectDataFromTheMethodAddAdopter() {
+        assertEquals(adopterCorrect, adopter);
+    }
+
+    @Test
+    public void CheckingForIncomingWrongDataFromTheMethodAddAdopter() {
+        assertNotEquals(adopterCorrect, adopterWrong);
+    }
 
     @Test
     public void shouldAddUserWithParameters() {
-        user = new User(CORRECT_USER_ID, CORRECT_USER_NAME, CORRECT_USER_TYPE, CORRECT_USER_STATUS);
 
         assertNotNull(user.getUserId());
         assertNotNull(user.getTelegramNick());
@@ -42,39 +125,32 @@ class UserTest {
 
     @Test
     public void shouldAddGuestWithParameters() {
-        user = new User(CORRECT_USER_ID, CORRECT_USER_NAME, CORRECT_FIRST_NAME,
-                CORRECT_LAST_NAME, CORRECT_PHONE_NUMBER, CORRECT_CAR_NUMBER,
-                CORRECT_SHELTER_TYPE, CORRECT_USER_TYPE, CORRECT_USER_STATUS);
 
-        assertNotNull(user.getUserId());
-        assertNotNull(user.getTelegramNick());
-        assertNotNull(user.getUserType());
-        assertNotNull(user.getUserStatus());
-        assertNotNull(user.getFirstName());
-        assertNotNull(user.getLastName());
-        assertNotNull(user.getPhoneNumber());
-        assertNotNull(user.getCarNumber());
-        assertNotNull(user.getShelterType());
+        assertNotNull(guest.getUserId());
+        assertNotNull(guest.getTelegramNick());
+        assertNotNull(guest.getUserType());
+        assertNotNull(guest.getUserStatus());
+        assertNotNull(guest.getFirstName());
+        assertNotNull(guest.getLastName());
+        assertNotNull(guest.getPhoneNumber());
+        assertNotNull(guest.getCarNumber());
+        assertNotNull(guest.getShelterType());
     }
 
     @Test
     public void shouldAddAdopterWithParameters() {
-        user = new User(CORRECT_USER_ID, CORRECT_USER_NAME, CORRECT_FIRST_NAME,
-                CORRECT_LAST_NAME, CORRECT_PHONE_NUMBER, CORRECT_CAR_NUMBER,
-                CORRECT_MAIL, CORRECT_ADDRESS,
-                CORRECT_SHELTER_TYPE, CORRECT_USER_TYPE, CORRECT_USER_STATUS);
 
-        assertNotNull(user.getUserId());
-        assertNotNull(user.getTelegramNick());
-        assertNotNull(user.getUserType());
-        assertNotNull(user.getUserStatus());
-        assertNotNull(user.getFirstName());
-        assertNotNull(user.getLastName());
-        assertNotNull(user.getPhoneNumber());
-        assertNotNull(user.getCarNumber());
-        assertNotNull(user.getShelterType());
-        assertNotNull(user.getAddress());
-        assertNotNull(user.getEmail());
+        assertNotNull(adopter.getUserId());
+        assertNotNull(adopter.getTelegramNick());
+        assertNotNull(adopter.getUserType());
+        assertNotNull(adopter.getUserStatus());
+        assertNotNull(adopter.getFirstName());
+        assertNotNull(adopter.getLastName());
+        assertNotNull(adopter.getPhoneNumber());
+        assertNotNull(adopter.getCarNumber());
+        assertNotNull(adopter.getShelterType());
+        assertNotNull(adopter.getAddress());
+        assertNotNull(adopter.getEmail());
     }
 
     @Test
@@ -96,12 +172,9 @@ class UserTest {
 
     @Test
     public void shouldAddGuestWithParametersIsBlank() {
-        user = new User(CORRECT_USER_ID, BLANK_VALUE, CORRECT_FIRST_NAME,
-                CORRECT_LAST_NAME, CORRECT_PHONE_NUMBER, BLANK_VALUE,
-                CORRECT_SHELTER_TYPE, CORRECT_USER_TYPE, CORRECT_USER_STATUS);
 
-        assertEquals(user.getCarNumber(), NOT_CAR);
-        assertEquals(user.getTelegramNick(), ConstantValue.DEFAULT_TELEGRAM_NICK);
+        assertEquals(guestWrong.getCarNumber(), NOT_CAR);
+        assertEquals(guestWrong.getTelegramNick(), ConstantValue.DEFAULT_TELEGRAM_NICK);
     }
 
     @Test
